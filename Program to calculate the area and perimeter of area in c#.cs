53aa43f7ff90b1f1ed482965c8d525f4b883b9c5
sandeep084991904 <sandeep084991904@gmail.com>
2024-08-10 17:43:16 +0530

    Program of calcuate Area and Perimeter of given Rectangle

---
+++ Program to calculate the area and perimeter of area in c#.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Programs_in_c_sharp
+{
+    public class Rectangle
+    {
+
+        /// <summary>
+        /// Program of calcuate Area and Perimeter of given Rectangle
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        public static void CalculateAreaAndPerimeter()
+        {
+
+            try
+            {
+
+            LengthStart:
+                Console.Write("Enter the length value : ");
+
+                if (!Decimal.TryParse(Console.ReadLine(), out Decimal Length))
+                {
+                    
+                    Console.WriteLine("Length value is should be in number.");
+                    goto LengthStart;
+                }
+            WidthStart:
+                Console.Write("Enter the width value : ");
+                if (!Decimal.TryParse(Console.ReadLine(), out Decimal Width))
+                {
+                    Console.WriteLine("Width value is should be in number.");
+                    goto WidthStart;
+                    
+
+                }
+                //Formula of Area=Length*Width
+
+                decimal Area = (Length * Width);
+
+                //Formula of Perimeter=2*(Length + Width)
+
+                decimal Perimeter = 2 * (Length + Width);
+
+                Console.WriteLine($"Area of length:{Length} and width:{Width}= {Area}");
+                Console.WriteLine($"Perimeter of length:{Length} and width:{Width}= {Perimeter}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
